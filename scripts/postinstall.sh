6f88d514a768906e1eddbf1ccd6a850d523ab618
Julian Mulder <git@nerdz.za.net>
2016-02-11 09:49:50 +0200

    mode deploy changes

--- scripts/postinstall.sh
+++ scripts/postinstall.sh
@@ -2,6 +2,6 @@ manage="${VENV}/bin/python ${INSTALLDIR}/${NAME}/manage.py"
 echo "Manage command: $manage" >> /var/praekelt/postinstall.log
 
 echo "Running syncdb" >> /var/praekelt/postinstall.log
-$manage syncdb --noinput --no-initial-data --migrate --settings=project.settings_live_web_site >> /var/praekelt/postinstall.log 2>&1
+$manage syncdb --noinput --no-initial-data --migrate  >> /var/praekelt/postinstall.log 2>&1
 
 echo "A deployment to `hostname` has been completed" | mail -s "Telkom Deployment: `hostname`" 1telkom@praekelt.com
